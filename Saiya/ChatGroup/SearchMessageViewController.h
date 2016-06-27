cf63254e82d095c331718aa53a180942c8b480f1
gyor@home <zougyor@zougyordeMacBook-Pro.local>
2016-06-27 23:33:04 +0800

    XX

---
+++ Saiya/ChatGroup/SearchMessageViewController.h
@@ -0,0 +1,21 @@
+/************************************************************
+ *  * Hyphenate CONFIDENTIAL
+ * __________________
+ * Copyright (C) 2016 Hyphenate Inc. All rights reserved.
+ *
+ * NOTICE: All information contained herein is, and remains
+ * the property of Hyphenate Inc.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Hyphenate Inc.
+ */
+
+#import <UIKit/UIKit.h>
+#import "EMClient.h"
+#import "EMClientDelegate.h"
+@interface SearchMessageViewController : UIViewController
+
+- (instancetype)initWithConversationId:(NSString *)conversationId
+                      conversationType:(EMConversationType)conversationType;
+
+@end
---
+++ Saiya/Contacts/FriendApply/AddFriendViewController.h
@@ -0,0 +1,20 @@
+/************************************************************
+  *  * Hyphenate CONFIDENTIAL 
+  * __________________ 
+  * Copyright (C) 2016 Hyphenate Inc. All rights reserved. 
+  *  
+  * NOTICE: All information contained herein is, and remains 
+  * the property of Hyphenate Inc.
+  * Dissemination of this information or reproduction of this material 
+  * is strictly forbidden unless prior written permission is obtained
+  * from Hyphenate Inc.
+  */
+
+#import <UIKit/UIKit.h>
+
+#import "EMSDK.h"
+#import "EMClient.h"
+#import "EMClientDelegate.h"
+@interface AddFriendViewController : UITableViewController
+
+@end
