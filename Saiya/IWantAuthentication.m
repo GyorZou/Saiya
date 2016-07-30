eff9c1106173fddf9f946a1bb642ae51340ed63d
gyor@home <zougyor@zougyordeMacBook-Pro.local>
2016-07-30 17:35:02 +0800

    阿萨德

---
+++ Saiya/IWantAuthentication.m
@@ -0,0 +1,33 @@
+//
+//  IWantAuthentication.m
+//  Saiya
+//
+//  Created by zougyor on 16/7/30.
+//  Copyright © 2016年 crv. All rights reserved.
+//
+
+#import "IWantAuthentication.h"
+
+@implementation IWantAuthentication
+
+-(IBAction)doAuthen:(UIButton*)sender
+{
+    long tag = sender.tag;
+    if (tag == 0) {//个人
+        
+    }else{
+    
+    }
+}
+
+-(IBAction)doShowDetail:(UIButton*)sender
+{
+    long tag = sender.tag;
+    if (tag == 0) {//官方
+        
+    }else{
+        
+    }
+}
+
+@end
